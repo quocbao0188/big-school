d91552608b4e6fe138d69b9316c1e11e28bd77bd
Huynh Quoc Bao <huynhquocbao0188@gmail.com>
2020-06-05 13:24:15 +0700

    Init project

---
+++ LAB456/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using LAB456.Models;
+using LAB456.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LAB456.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
